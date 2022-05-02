470e739efed660a221b52a377850fd03c88e0f9b
Maris Lagzdins <maaris.lagzdins@gmail.com>
2022-05-02 15:10:42 +0300

    Update iosrssi to 1.6.0

--- iosrssi.rb
+++ iosrssi.rb
@@ -1,8 +1,8 @@
 class Iosrssi < Formula
   desc "A Swift command-line tool to parse iOS device sysdiagnose log files and retrieve the wifi network RSSI statistics."
   homepage "https://github.com/DeveloperMaris/iosrssi"
-  url "https://github.com/DeveloperMaris/iosrssi/archive/1.5.0.zip"
-  sha256 "f0fb90ecbec4259c01fc4ed8b91bcdc77b1b9e8d9820c0a29f807e5d91d1a5dc"
+  url "https://github.com/DeveloperMaris/iosrssi/archive/1.6.0.zip"
+  sha256 "dd5a088b714898e3abfc76a2d5cd47f6624296eb88a9808ba6a89831a3d05a33"
   license "MIT"
 
   def install
